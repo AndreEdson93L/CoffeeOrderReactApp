f91ea8421ad19792561de690fb1e187b9dc851c0
Andrea Edson Lorenzoni <andrea@gmail.com>
2023-02-21 01:32:35 +0000

    Sample project. following part 13, 14, 15, 16, 17.

---
+++ src/components/Orders/OrdersForm.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function OrdersForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit()}>
+      <fieldset>
+        <label htmlFor="order-notes">Order notes:</label>
+        <input type="text" {...register("orderNotes")} placeholder="No sugar, extra milk"/>
+      </fieldset>
+
+      <button type="submit">Order</button>
+    </form>
+  );
+}
+
+export default OrdersForm;
--- src/views/Orders.jsx
+++ src/views/Orders.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import withAuth from '../hoc/withAuth'
+import React, { useState } from "react";
+import OrdersForm from "../components/Orders/OrdersForm";
+import withAuth from "../hoc/withAuth";
+
+const COFFEES = [
+  {
+    id: 1,
+    name: "Coffee1",
+    image: "img/coffee1.png",
+    alt: "coffee",
+  },
+  {
+    id: 2,
+    name: "Coffee2",
+    image: "img/coffee2.png",
+    alt: "coffee",
+  },
+  {
+    id: 3,
+    name: "Coffee3",
+    image: "img/coffee3.png",
+    alt: "coffee",
+  },
+  {
+    id: 4,
+    name: "Coffee4",
+    image: "img/coffee4.png",
+    alt: "coffee",
+  },
+];
 
 const Orders = () => {
-    return (
-        <h1>Orders</h1>
-    )
-}
 
-export default withAuth(Orders)
+  const [coffee, setCoffee] = useState(null)
+
+  const handleCoffeeClicked = (coffeeId) => {
+    const selectedCoffee = COFFEES.find(coffee => coffee.id === coffeeId )
+    setCoffee(selectedCoffee)
+  };
+
+  const availableCoffees = COFFEES.map((coffee) => {
+    return (
+      <OrdersCoffeeButton
+        key={coffee.id}
+        coffee={coffee}
+        onSelect={handleCoffeeClicked}
+      />
+    );
+  });
+
+  return (
+    <>
+      <h1>Orders</h1>
+      <section id="coffee-options">{availableCoffees}</section>
+      <p>Summary: </p>
+      {coffee && <p>Selected Coffee: {coffee.name}</p>}
+    </>
+  );
+};
+
+export default withAuth(Orders);
